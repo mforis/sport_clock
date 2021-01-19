d3273e2cb34f471bc358ad81d46042948a4ec4ee
SerZheka <genjkaser@yandex.by>
2021-01-20 01:11:20 +0300

    Перевод светодиодов часов на библиотеку microlib

--- Clock-WS2812_with_button.ino
+++ Clock-WS2812_with_button.ino
@@ -1,6 +1,6 @@
-// Developed by MAX_FORYS
+// Developed by MAX_FORYS and SerZheka
 // 09.07.2020
-// Ver. 1.3.0
+// Ver. 1.3.2
 
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -10,6 +10,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <GyverButton.h>
 #include <RTClib.h>
+#include <microLED.h>
 
 #define SENSOR A0           // Фоторезистор
 #define RADIOTRANSMITION 0  // 0 - выключить передачу, 1 - включить
@@ -17,11 +18,11 @@
 #define NUM_LEDS 174        // 5 by segment + 6 in the middle
 #define PIXEL_SEGMENT 29    // количество светодиодов в цифре
 #define PIN 6               // Data pin for led comunication
-#define BUZ 7
+#define BUZZ 7
 #define AUTOBRIGHTNESS 0
 #define DEBUG 0 // 0 - без вывода в консоль, 1 - с выводом
-// LED_TYPE WS2812
-// COLOR_ORDER GRB
+#define LED_TYPE LED_WS2812
+#define COLOR_ORDER ORDER_GRB
 
 const byte digits[10][29] = {
     {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -35,14 +36,17 @@ const byte digits[10][29] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1}};
 
-const long ColorTable[7]{
-    0xFF0000,
+const int ColorTable[6]{
     0xFF0000,
     0xFFFF00,
     0x008000,
     0x00FFFF,
     0x0000FF,
     0x800080};
+
+enum Colors {
+    colWhite = 0xFFFFFF
+};
 
 RF24 radio(5, 3);
 GButton Butt1(8);
@@ -55,32 +59,36 @@ int ledLevel = 255;
 int mode = 1;                                                                       // Текущий режим настройки часов
 int color = 0;                                                                      // Текущая яркость часов
 int setmode = 0;                                                                    // Режим работы (0 - соревновательный режим, 1 - режим часов)
-Adafruit_NeoPixel strip_a = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800); // Определение полосы светодиодов
+// Adafruit_NeoPixel strip_a = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800); // Определение полосы светодиодов
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, 4, NEO_GRB + NEO_KHZ800);            // Моргающие светодиодики
 long ledColor = 0xFF0000;
+microLED<NUM_LEDS, PIN, MLED_NO_CLOCK, LED_TYPE, COLOR_ORDER> mainLEDs;
 
 void setup()
 {
-    if (RADIOTRANSMITION)
-    {
-        radio.begin();
-        delay(2);
-        radio.setChannel(9);
-        // скорость, RF24_250KBPS, RF24_1MBPS или RF24_2MBPS (RF24_250KBPS на nRF24L01 (без +) неработает).
-        // меньше скорость, выше чувствительность приемника.
-        radio.setDataRate(RF24_1MBPS);
-        // мощность передатчика, RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBM,
-        radio.setPALevel(RF24_PA_HIGH); // TODO есть еще RF24_PA_MAX, знаешь?
-        radio.openWritingPipe(PIPE);
-    }
+#if (RADIOTRANSMITION)
+    radio.begin();
+    delay(2);
+    radio.setChannel(9);
+    // скорость, RF24_250KBPS, RF24_1MBPS или RF24_2MBPS (RF24_250KBPS на nRF24L01 (без +) неработает).
+    // меньше скорость, выше чувствительность приемника.
+    radio.setDataRate(RF24_1MBPS);
+    // мощность передатчика, RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBM,
+    radio.setPALevel(RF24_PA_HIGH); // TODO есть еще RF24_PA_MAX, знаешь?
+    radio.openWritingPipe(PIPE);
+#endif
 
     strip.setBrightness(255);
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'
 
-    strip_a.setBrightness(ledLevel);
-    strip_a.begin();
-    strip_a.show(); // Initialize all pixels to 'off'
+    // strip_a.setBrightness(ledLevel);
+    // strip_a.begin();
+    // strip_a.show(); // Initialize all pixels to 'off'
+
+    mainLEDs.setBrightness(ledLevel);
+    mainLEDs.clear();
+    mainLEDs.show();
 
     Butt1.setDebounce(80);      // настройка антидребезга
     Butt1.setClickTimeout(300); // настройка таймаута между кликами
@@ -92,7 +100,7 @@ void setup()
     if (DEBUG)
         Serial.begin(9600);
     rtc.begin();
-    pinMode(BUZ, OUTPUT);
+    pinMode(BUZZ, OUTPUT);
     long ledColor = 0xffdd33; // TODO зачем? А главное почему?
     pinMode(SENSOR, INPUT);
 }
@@ -116,9 +124,15 @@ void displayLed(int Now, int cursor, int cursor1)
         for (int k = 0; k < PIXEL_SEGMENT; k++)
         {
             if (digits[digit][k])
-                strip_a.setPixelColor(cursor, ledColor);
+            {
+                // strip_a.setPixelColor(cursor, ledColor);
+                mainLEDs[cursor] = ledColor;
+            }
             else
-                strip_a.setPixelColor(cursor, 0);
+            {
+                // strip_a.setPixelColor(cursor, 0);
+                mainLEDs[cursor] = 0;
+            }
             cursor++;
         }
         cursor = cursor1;
@@ -147,7 +161,7 @@ void WarningStartDelay()
         for (int i = 0; i < 4; i++)
             strip.setPixelColor(i, strip.Color(0, 255, 0));
         strip.show();
-        digitalWrite(BUZ, HIGH);
+        digitalWrite(BUZZ, HIGH);
         ledState = LOW;
     }
 
@@ -156,7 +170,7 @@ void WarningStartDelay()
         for (int i = 0; i < 4; i++)
             strip.setPixelColor(i, strip.Color(255, 0, 0));
         strip.show();
-        digitalWrite(BUZ, LOW);
+        digitalWrite(BUZZ, LOW);
     }
 
     if ((currentMillis - previousMillis > 399) && (now.second() >= 57))
@@ -167,14 +181,14 @@ void WarningStartDelay()
         {
             for (int i = 0; i < 4; i++)
                 strip.setPixelColor(i, strip.Color(0, 0, 0));
-            noTone(BUZ);
+            noTone(BUZZ);
             ledState = HIGH;
         }
         else
         {
             for (int i = 0; i < 4; i++)
                 strip.setPixelColor(i, strip.Color(255, 255, 0));
-            digitalWrite(BUZ, HIGH);
+            digitalWrite(BUZZ, HIGH);
             ledState = LOW;
         }
         strip.show();
@@ -205,7 +219,10 @@ void ButtonControl()
 
     if (Butt1.isClick())
     {
-        mode++;
+        if (mode >= 4)
+            mode = 1;
+        else
+            mode++;
         if (DEBUG)
         {
             Serial.print("Click ");
@@ -222,27 +239,24 @@ void ButtonControl()
         if (Butt2.isClick())
         {
             ledLevel += 51;
-            if (DEBUG)
-            {
-                Serial.print("Текущая яркость: ");
-                Serial.println(ledLevel);
-            }
             if (ledLevel > 255)
                 ledLevel = 0; // TODO а может лучше 255?
-            strip_a.setBrightness(ledLevel);
+            // strip_a.setBrightness(ledLevel);
+            mainLEDs.setBrightness(ledLevel);
         }
         // если кнопка была удержана (это для декремента яркости)
         if (Butt3.isClick())
         {
             ledLevel -= 51;
-            if (DEBUG)
-            {
-                Serial.print("Текущая яркость: ");
-                Serial.println(ledLevel);
-            }
             if (ledLevel < 0)
                 ledLevel = 255; // TODO а тут 0?
-            strip_a.setBrightness(ledLevel);
+            // strip_a.setBrightness(ledLevel);
+            mainLEDs.setBrightness(ledLevel);
+        }
+        if (DEBUG)
+        {
+            Serial.print("Текущая яркость: ");
+            Serial.println(ledLevel);
         }
         break;
 
@@ -250,9 +264,10 @@ void ButtonControl()
         if (DEBUG)
             Serial.println("Настройка цвета");
         if (Butt2.isClick())
-            color++;
-        if (color > 6)
-            color = 1;
+            if (color >= sizeof(ColorTable) / sizeof(int))
+                color = 1;
+            else
+                color++;
         ledColor = ColorTable[color];
         break;
 
@@ -308,7 +323,8 @@ void ButtonControl()
         break;
 
     default:
-        mode = 1;
+        if (DEBUG)
+            Serial.println("Ошибка настройки часов");
         break;
     }
 }
@@ -316,20 +332,27 @@ void ButtonControl()
 void brightnessAuto()
 {
     ledLevel = analogRead(SENSOR) / 4; // Полученные значения на аналоговом входе A0 делим на 4
-    strip_a.setBrightness(ledLevel);
+    // strip_a.setBrightness(ledLevel);
+    mainLEDs.setBrightness(ledLevel);
     if (DEBUG)
+    {
+        Serial.print("Яркость после автонастройки ");
         Serial.println(ledLevel);
+    }
 }
 
 void loop()
 {
-    if (RADIOTRANSMITION)
-        nRF24L01();
+#if (RADIOTRANSMITION)
+    nRF24L01();
+#endif
     ButtonControl();
-    if (AUTOBRIGHTNESS)
-        brightnessAuto();
+#if (AUTOBRIGHTNESS)
+    brightnessAuto(); // TODO  может перенести в setup? Типа включил, они настроились на свет, а потом если надо вручную регулируешь
+#endif
     if (!setmode)
         WarningStartDelay();
     TimeToArray();
-    strip_a.show();
+    // strip_a.show();
+    mainLEDs.show();
 }
